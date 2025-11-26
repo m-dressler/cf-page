1462bec5c956c2a34c129be91dff1e1d1e1ac51d
Mauritz Dressler <mauritz@prodport.com>
2025-11-26 11:30:30 +0000

    📈 src | bundle.ts | Use API rather than CLI

--- src/util/bundle.ts
+++ src/util/bundle.ts
@@ -1,26 +1,19 @@
-import { CONFIG } from "../config.ts";
 
 /** Uses `deno bundle` since there's no runtime API available yet (https://docs.deno.com/runtime/reference/bundling/) */
 export const bundle = async (
   filePath: string | URL,
   minify: boolean,
 ): Promise<string> => {
-  const cmd = new Deno.Command(Deno.execPath(), {
-    args: [
-      "bundle",
-      "--unstable-raw-imports",
-      CONFIG.denoJsonFilePath && "--config",
-      CONFIG.denoJsonFilePath, // Auto-discovers project config with import maps
-      minify && "--minify",
-      filePath.toString(),
-    ].filter((arg) => arg !== false && arg != null),
-    stdout: "piped",
-    stderr: "piped",
+  const result = await Deno.bundle({
+    entrypoints: [filePath.toString()],
+    minify,
   });
 
-  const { success, stdout, stderr } = await cmd.output();
-  if (success) return new TextDecoder().decode(stdout);
+  if (!result.success) {
+    throw new Error(`Deno bundle failed for ${filePath}`, {
+      cause: result.errors,
+    });
+  }
 
-  const errorText = new TextDecoder().decode(stderr);
-  throw new Error(`Deno bundle failed for ${filePath}`, { cause: errorText });
+  return new TextDecoder().decode(result.outputFiles![0]!.contents!);
 };
