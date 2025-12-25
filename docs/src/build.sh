a0274844432d603f28e1219fc466ac0f10de8570
Mauritz Dressler <mauritz@prodport.com>
2025-12-25 12:41:25 +0000

    📈 docs | build.sh | Add `VERBOSE` flag to control dependency installation step logs

--- docs/src/build.sh
+++ docs/src/build.sh
@@ -7,9 +7,30 @@ export DENO_INSTALL=".cache/deno"
 export DENO_DIR=".cache/deno_dir"
 export DENO_BIN="$DENO_INSTALL/bin/deno"
 
+VERBOSE=false
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -v|--verbose)
+      VERBOSE=true
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
 # Install Deno if not present
 if [ ! -x $DENO_BIN ]; then
   curl -fsSL https://deno.land/install.sh | sh -s -- -y --no-modify-path
+fi
+
+# Install dependencies using verbosity flag for download logs
+if [[ "$VERBOSE" == true ]]; then
+  $DENO_BIN install
+else
+  $DENO_BIN install --quiet
 fi
 
 # Run build script
